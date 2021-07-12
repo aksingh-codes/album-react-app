dc188269020d5dacd0836dbf8c40bf34b91d8d0d
aksingh47 <56232421+aksingh47@users.noreply.github.com>
2021-07-12 18:13:55 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from 'react';
+import Card from "./Card";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    const [albumData, setAlbumData] = useState([]);
+
+    const fetchData = () => {
+        return fetch("http://jsonplaceholder.typicode.com/album/2/photos")
+              .then((response) => response.json())
+              .then((data) => {
+                setAlbumData(data);
+                
+            });
+    }
+    
+
+    const removeCard = (id)=> {
+        setAlbumData(
+            albumData.filter(
+            data => (data.id !== id)
+        ))
+    }
+    
+    return (
+        <div style={{textAlign:"center"}}>
+            <button className="fetch-btn" onClick={fetchData}>
+                Fetch
+            </button>
+            <div className="album-grid">
+            {
+                albumData.map(
+                    data => {
+                        return <Card key={data.id} removeCard={removeCard} data={data} />
+                    }
+                )
+                
+            }
+            </div>
+
+            {
+                <p className="total-cards">
+                    {
+                        "Total Cards : " + albumData.length
+                    }
+                </p>
+            }
+            
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Card(props) {
+    
+    const data = props.data;
+    
+    return (
+        <div className="card" onClick={
+            () => {
+                props.removeCard(data.id)
+            }
+        }>
+            <img className="card-img" src= {data.url} alt="" />
+            <p className="card-title">
+                {data.title}
+            </p>
+        </div>
+    )
+}
+
+export default Card
